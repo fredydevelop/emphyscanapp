1c469df246341a8f433c7114bd84abd9a65e8edd
Coded Coder <105555015+fredydevelop@users.noreply.github.com>
2026-05-30 14:45:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def insert():
         # To load the model
         try:
             loaded_model = load_model(
-                "main_emphysema_model.keras",
+                "try_emphysema_model.keras",
                 compile=False
             )
         except Exception:
